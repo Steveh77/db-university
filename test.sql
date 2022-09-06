f693d48720a120b03bd3bfc74dc3c2b962c597fa
Steveh77 <stederosa76@gmail.com>
2022-09-06 16:59:39 +0200

    Added "QUERY CON SELECT" es 6-7-8

--- test.sql
+++ test.sql
@@ -32,12 +32,20 @@ WHERE `date`LIKE "2020-06-20" AND `hour`>"14:00:00";
 
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT *
+FROM `degrees`
+WHERE `level` LIKE "magistrale";
 
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
+SELECT * 
+FROM `departments`
 
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT *
+FROM `teachers`
+WHERE `phone` IS NULL;
 
 
 
