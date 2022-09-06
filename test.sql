07544fef03ec1c19cbefb3073fa2b3818a12ce5d
Steveh77 <stederosa76@gmail.com>
2022-09-06 18:04:20 +0200

    Added "QUERY CON GROUP BY" es 1-2

--- test.sql
+++ test.sql
@@ -53,9 +53,16 @@ WHERE `phone` IS NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT YEAR(`enrolment_date`), COUNT(*) 
+FROM `students`
+WHERE `enrolment_date` >"2018-%-%"
+GROUP BY YEAR (`enrolment_date`);
 
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT `office_address`, COUNT(`office_address`) 
+FROM `teachers`  
+GROUP BY `office_address`;
 
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
